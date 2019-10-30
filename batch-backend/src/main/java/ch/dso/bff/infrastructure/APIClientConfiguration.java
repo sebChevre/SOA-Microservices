69a6b552fd19cdc61ff87062b875e353c6166d96
Sébastien Chèvre <seb.chevre@gmail.com>
2019-10-30 10:43:41 +0100

    ok ok

---
+++ batch-backend/src/main/java/ch/dso/bff/infrastructure/APIClientConfiguration.java
@@ -0,0 +1,19 @@
+package ch.dso.bff.infrastructure;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class APIClientConfiguration {
+
+    @LoadBalanced
+    @Bean
+    public RestTemplate apiClient(){
+        return new RestTemplate();
+    }
+
+
+}
